adb0622ca464e66dea3d7bfbc2c10d21b7ebace0
CloudLam <cloud.zero@qq.com>
2018-03-17 21:52:38 +0800

    table#0

--- src/component/util/table.js
+++ src/component/util/table.js
@@ -7,11 +7,37 @@ function Table(object) {
     node: object.node || null,
     col: [],
     row: 0,
+    max: object.max || 10,
     current: 1,
-    init: _init
+    init: _init,
+    sort: _sort,
+    first: _first,
+    prev: _prev,
+    next: _next,
+    last: _last,
+    jump: _jump,
+    search: _search
   };
 
   function _init () {}
+
+  function _sort () {}
+
+  function _first () {}
+
+  function _prev () {}
+
+  function _next () {}
+
+  function _last () {}
+
+  function _jump () {}
+
+  function _search () {}
+
+  function _draw () {}
+
+  function _clear () {}
 
   return table;
 }
