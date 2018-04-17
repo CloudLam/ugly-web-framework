9621f4138d9d2ffd659eb8f88b127b633664d458
CloudLam <cloud.zero@qq.com>
2018-04-17 21:20:41 +0800

    page#0

--- src/component/util/datepicker.js
+++ src/component/util/datepicker.js
@@ -13,17 +13,26 @@ function DatePicker(object) {
     now: null
   };
 
-  var date = '<div class="' + picker.class + '"></div>';
+  var date_head = '<div><div><span>&#9664;</span><span></span><span>&#9654;</span></div>';
+  var date_foot = '<div><span>' + 'Pick Time' + 
+    '</span><button>' + 'Today' + 
+    '</button><button>' + 'Reset' + 
+    '</button></div></div>';
 
-  var time = '<div class="' + picker.class + '"></div>';
+  var time_body = '<div></div>';
+  var time_foot = '<div></div>';
 
   _init.call(picker);
 
   function _init() {
+    this.now = new Date();
     this.node = document.createElement('div');
     this.node.style.position = 'absolute';
     this.node.style.zIndex = '999';
-    this.node.innerHTML = date + time;
+    this.node.setAttribute('class', this.class);
+    this.node.innerHTML = date_head + 
+      '<div>' + _calendar(this.now.getFullYear(), this.now.getMonth(), 1) + '</div>' + 
+      date_foot;
   }
 
   function _setPosition(target) {
@@ -32,6 +41,68 @@ function DatePicker(object) {
       target.parentNode.offsetLeft - 
       parseInt(getStyle(target.parentNode, 'padding-left').replace('px', '')) + 
       'px';
+  }
+
+  function _isLeapYear (year) {
+    return year % 100 == 0 ? (year % 400 == 0 ? 1 : 0) : (year % 4 == 0 ? 1 : 0);
+  }
+  
+  function _calendar (year, month) {
+    var firstday = new Date(year, month, 1);
+    var days = new Array(31, 28 + _isLeapYear(year), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+    var mlast = month ? month - 1 : 11;
+    var calendar = '<table><thead><tr>' + 
+      '<th>' + 'Su' + '</th>' +
+      '<th>' + 'Mo' + '</th>' +
+      '<th>' + 'Tu' + '</th>' +
+      '<th>' + 'We' + '</th>' +
+      '<th>' + 'Th' + '</th>' +
+      '<th>' + 'Fr' + '</th>' +
+      '<th>' + 'Sa' + '</th>' +
+      '</tr><tbody><tr>';
+    for (var i = firstday.getDay(); i > 0; i--) {
+      calendar += '<td style="opacity:0.3">' + (days[mlast] - i + 1) + '</td>';
+    }
+    for (var j = 1; j <= days[month]; j++) {
+      calendar += '<td>' + j + '</td>';
+      if ((j + firstday.getDay()) % 7 == 0) {
+        if (j == days[month]) {
+          calendar += '</tr>';
+        } else {
+          calendar += '</tr><tr>';
+        }
+      }
+    }
+    if ((days[month] + firstday.getDay()) % 7 != 0) {
+      for (var k = 1; k <= 7 - (days[month] + firstday.getDay()) % 7; k++) {
+        calendar += '<td style="opacity:0.3">' + k + '</td>';
+      }
+      calendar += '</tr>';
+    }
+    calendar += '</tbody></table>';
+    return calendar;
+  }
+
+  function _timepicker () {
+    var hSelection = '<label>H</label><select>';
+    for (var h = 0; h < 24; h++) {
+      hSelection += '<option>' + h + '</option>';
+    }
+    hSelection += '</select>';
+
+    var mSelection = '<label>M</label><select>';
+    for (var m = 0; m < 60; m++) {
+      mSelection += '<option>' + m + '</option>';
+    }
+    mSelection += '</select>';
+
+    var sSelection = '<label>S</label><select>';
+    for (var s = 0; s < 60; s++) {
+      sSelection += '<option>' + s + '</option>';
+    }
+    sSelection += '</select>';
+
+    return hSelection + mSelection + sSelection;
   }
 
   var datePickerHandler = function(event) {
