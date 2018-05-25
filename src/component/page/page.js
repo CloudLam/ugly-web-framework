b3a524664674d50ec2632e2371b768998057c1bb
CloudLam <cloud.zero@qq.com>
2018-05-25 16:48:34 +0800

    page#0

--- src/component/page/page.js
+++ src/component/page/page.js
@@ -3,54 +3,56 @@
 'use strict';
 
 function settingInit(app) {
-  // Current sidebar type
-  if (localStorage.getItem('uwfAppType')) {
-    if (parseInt(localStorage.getItem('uwfAppType'))) {
-      document.getElementById('uwf-setting-sidebartype-d').setAttribute('checked', '');
-    } else {
-      document.getElementById('uwf-setting-sidebartype-s').setAttribute('checked', '');
+  var sidebarType = document.getElementById('uwf-setting-sidebartype');
+  var sidebarColor = document.getElementById('uwf-setting-sidebarcolor');
+
+  var listHandler = function (event) {
+    if (event.target == sidebarType || event.target.parentNode == sidebarType) {
+      closeList([sidebarColor]);
+      openList(sidebarType);
+      return;
     }
-  } else if (app.type) {
-    document.getElementById('uwf-setting-sidebartype-d').setAttribute('checked', '');
-  } else {
-    document.getElementById('uwf-setting-sidebartype-s').setAttribute('checked', '');
+    if (event.target == sidebarColor || event.target.parentNode == sidebarColor) {
+      closeList([sidebarType]);
+      openList(sidebarColor);
+      return;
+    }
+
+    var className = event.target.parentNode.getAttribute('class');
+    if (className && className.indexOf('uwf-setting-option') > -1) {
+      var select = event.target.parentNode.previousSibling || event.target.parentNode.previousSiblingElement;
+      selectOption(select, event.target.innerHTML);
+    }
+
+    closeList([sidebarType, sidebarColor]);
+  };
+
+  document.removeEventListener('click', listHandler, false);
+  document.addEventListener('click', listHandler, false);
+
+  function openList (element) {
+    element.parentNode.setAttribute('class', element.parentNode.getAttribute('class').replace('hide', 'show'));
   }
 
-  // Current sidebar color
-  document.getElementById('uwf-setting-sidebarcolor-' + app.sidebar.color).setAttribute('checked', '');
+  function closeList (elements) {
+    for (var i = 0; i < elements.length; i++) {
+      elements[i].parentNode.setAttribute('class', elements[i].parentNode.getAttribute('class').replace('show', 'hide'));
+    }
+  }
 
-  // Set local
-  document.getElementById('uwf-setting-sidebartype-s').onclick = function (event) {
-    app.type = 0;
-    localStorage.setItem('uwfAppType', app.type);
-  }
-  document.getElementById('uwf-setting-sidebartype-d').onclick = function (event) {
-    app.type = 1;
-    localStorage.setItem('uwfAppType', app.type);
-  }
-  document.getElementById('uwf-setting-sidebarcolor-blue').onclick = function (event) {
-    app.sidebar.color = 'blue';
-    localStorage.setItem('uwfAppColor', app.sidebar.color);
-  }
-  document.getElementById('uwf-setting-sidebarcolor-red').onclick = function (event) {
-    app.sidebar.color = 'red';
-    localStorage.setItem('uwfAppColor', app.sidebar.color);
-  }
-  document.getElementById('uwf-setting-sidebarcolor-green').onclick = function (event) {
-    app.sidebar.color = 'green';
-    localStorage.setItem('uwfAppColor', app.sidebar.color);
-  }
-  document.getElementById('uwf-setting-sidebarcolor-yellow').onclick = function (event) {
-    app.sidebar.color = 'yellow';
-    localStorage.setItem('uwfAppColor', app.sidebar.color);
-  }
-  document.getElementById('uwf-setting-sidebarcolor-gray').onclick = function (event) {
-    app.sidebar.color = 'gray';
-    localStorage.setItem('uwfAppColor', app.sidebar.color);
-  }
-  document.getElementById('uwf-setting-sidebarcolor-black').onclick = function (event) {
-    app.sidebar.color = 'black';
-    localStorage.setItem('uwfAppColor', app.sidebar.color);
+  function selectOption(select, option) {
+    if (select == sidebarType) {
+      sidebarType.children[0].innerHTML = option;
+      app.type = option.toLowerCase() === 'detail' ? 1 : 0;
+      localStorage.setItem('uwfAppType', app.type);
+      return;
+    }
+    if (select == sidebarColor) {
+      sidebarColor.children[0].innerHTML = option;
+      app.sidebar.color = parseDOM(option)[0].className.split(' ')[1];
+      localStorage.setItem('uwfAppColor', app.sidebar.color);
+      return;
+    }
   }
 }
 
